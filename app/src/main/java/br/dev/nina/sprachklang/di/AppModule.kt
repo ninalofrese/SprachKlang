35fda90703d1e8a907104b9a953fb028996a3fc7
Nina <26123557+ninalofrese@users.noreply.github.com>
2024-02-29 14:38:56 +0100

    Add DI for Room and Moshi

--- app/src/main/java/br/dev/nina/sprachklang/di/AppModule.kt
+++ app/src/main/java/br/dev/nina/sprachklang/di/AppModule.kt
@@ -1,11 +1,30 @@
 package br.dev.nina.sprachklang.di
 
+import android.content.Context
+import androidx.room.Room
+import br.dev.nina.sprachklang.dictionarysearch.data.source.db.DictionaryDatabase
+import com.squareup.moshi.Moshi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideDictionaryDatabase(@ApplicationContext appContext: Context): DictionaryDatabase {
+        return Room.databaseBuilder(appContext, DictionaryDatabase::class.java, "dictionaryDb")
+            .createFromAsset("databases/GermanAusspracheDictionary.db")
+            .build()
+    }
 
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().build()
+    }
 }
